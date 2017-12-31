dbdf19afe8ffe11ff8115afe4fdb2f84f0f53d70
DRAJ <davidrajnus@gmail.com>
2017-12-31 14:01:06 +0800

    added circular and test

---
+++ spec/circular_spec.rb
@@ -0,0 +1,46 @@
+require './circular/circular'
+
+RSpec.describe '#circular' do
+
+  it 'circular detects circular linked lists' do
+    l = LinkedList.new
+    a = Node.new('a')
+    b = Node.new('b')
+    c = Node.new('c')
+    
+    l.head = a
+    a.next = b
+    b.next = c
+    c.next = b
+
+    expect(circular(l)).to eq(true)
+  end
+
+  it 'circular detects circular linked lists linked at the head' do
+    l = LinkedList.new
+    a = Node.new('a')
+    b = Node.new('b')
+    c = Node.new('c')
+    
+    l.head = a
+    a.next = b
+    b.next = c
+    c.next = a
+
+    expect(circular(l)).to eq(true)
+  end
+
+  it 'circular detects non-circular linked lists' do
+    l = LinkedList.new
+    a = Node.new('a')
+    b = Node.new('b')
+    c = Node.new('c')
+    
+    l.head = a
+    a.next = b
+    b.next = c
+    c.next = nil
+
+    expect(circular(l)).to eq(false)
+  end
+end
